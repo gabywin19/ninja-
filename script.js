4c3d6bde1964bdfe4845e8f0aabd3ee78be38675
gabywin19 <gabrielacarmona782@gmail.com>
2023-04-19 22:44:34 -0400

    le agregue una herencia sensei

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ console.log(said.sayName());
 console.log(said.showStats());
 said.drinkSake();
 console.log(said);
+
+
+
+class Sensei extends Ninja{
+    constructor(nombre, salud, velocidad, fuerza, sabiduria){
+
+        super(nombre , salud, velocidad, fuerza);
+        this.sabiduria= sabiduria;
+
+    }
+
+    showStats(){
+        return(this)
+    }
+
+    speaksWindsdom() {
+        
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        this.drinkSake();
+        
+    }
+}
+
+
+const superSensei =  new Sensei ("Master Splinter", 200, 10, 10, 10)
+superSensei.speaksWindsdom();
+console.log(superSensei.showStats());
